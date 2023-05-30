389c2411c24399b90e50c6211756afb2e195db92
Maciej Łodziński <85826774+mflodzinski@users.noreply.github.com>
2023-05-30 22:34:40 +0200

    Add files via upload

---
+++ preprocess/french_preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from preprocess.preprocess import Preprocess
+FRENCH_DATA_PATH = "data/basic/french"
+
+
+class FrenchPreprocess(Preprocess):
+    """
+    The class responsible for preprocessing french data.
+    """
+
+    def __init__(self, path: str = FRENCH_DATA_PATH, language="en_US") -> None:
+        """
+        Constructs all the necessary attributes for the **FrenchPreprocess** class object.
+        :param path: path to dataset (*french* default)
+        :param language: selected language
+        """
+
+        prepared_data = self.prepare_french_data(path)
+        super().__init__(prepared_data, path, language)
+
+    @staticmethod
+    def prepare_french_data(path: str = FRENCH_DATA_PATH) -> pd.DataFrame:
+        """
+        Prepares french data for later preprocessing.
+        :param path: path to dataset (*french* default)
+        :return: prepared french data
+        """
+
+        df = pd.read_csv(path + "/french.csv", lines=True)
+        df = df[['text', 'label']]
+        return df
---
+++ preprocess/japanese_preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from preprocess.preprocess import Preprocess
+JAPANESE_DATA_PATH = "data/basic/japanese"
+
+
+class JapanesePreprocess(Preprocess):
+    """
+    The class responsible for preprocessing japanese data.
+    """
+
+    def __init__(self, path: str = JAPANESE_DATA_PATH, language="en_US") -> None:
+        """
+        Constructs all the necessary attributes for the **Japanese** class object.
+        :param path: path to dataset (*japanese* default)
+        :param language: selected language
+        """
+
+        prepared_data = self.prepare_japanese_data(path)
+        super().__init__(prepared_data, path, language)
+
+    @staticmethod
+    def prepare_japanese_data(path: str = JAPANESE_DATA_PATH) -> pd.DataFrame:
+        """
+        Prepares japanese data for later preprocessing.
+        :param path: path to dataset (*japanese* default)
+        :return: prepared japanese data
+        """
+
+        df = pd.read_csv(path + "/japanese.csv")
+        df = df[['text', 'label']]
+        return df
